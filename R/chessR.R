28a37d80199736270a9383cd682b52fdd3c0fda1
JaseZiv <jaseziv83@gmail.com>
2019-09-14 23:22:45 +1000

    included the first of the functions

---
+++ R/chessR.R
@@ -0,0 +1,11 @@
+#' chessR: A package for extracting and analysing chess game data
+#'
+#' This package is designed to aid in the extraction and analysis of game data from the popular chess.com
+#'
+#' @docType package
+#' @name chessR
+"_PACKAGE"
+
+utils::globalVariables("clean_pgn")
+utils::globalVariables(c("each_game", "Moves", "Event", "Date", "EndDate", "White", "Username", "Black", "WhiteElo", "BlackElo", "Result",
+                         "UserColour", "UserResult", "Termination", "UserOpponent", "ECOUrl", ""))
