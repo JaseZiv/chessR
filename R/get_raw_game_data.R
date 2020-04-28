73c96f369cf182e03dcd914e5bd59d6d633ee009
Jason <jason.zivkovic@reece.com.au>
2020-04-28 19:46:13 +1000

    raw data function now returns for list of players

--- R/get_raw_game_data.R
+++ R/get_raw_game_data.R
@@ -1,9 +1,9 @@
-#' Get Raw chess.com Game Data
+#' Get Single Player Raw chess.com Game Data
 #'
-#' This function returns the raw json data for a player's or list of players'
-#' chess.com data as a date frame
+#' This function returns the raw json data for a player's
+#' chess.com data as a data frame
 #'
-#' @param usernames A vector of a valid unsername or usernames from chess.com
+#' @param usernames A valid unsername from chess.com
 #'
 #' @import magrittr
 #' @import purrr
@@ -11,41 +11,63 @@
 #' @import dplyr
 #' @import tidyr
 #'
-#' @export
-get_raw_game_data <- function(usernames) {
-
-  cat("Extracting Data, please wait\n")
-  usernames <- usernames
+get_each_raw <- function(username) {
+  cat("Extracting ", username, " Data, please wait\n")
 
 
+
+  # this function gets a list of all year/months the player(s) has played on chess.com
   get_game_urls <- function(){
-
-    user_names <- usernames
-    part1 <- "https://api.chess.com/pub/player/"
-    part2 <- "/games/archives"
-
-    archive_url <- paste0(part1, user_names, part2)
-    archive_urls <- purrr::map(archive_url, jsonlite::fromJSON) %>% unlist() %>% unname()
-
-    return(archive_urls)
+    jsonlite::fromJSON(paste0("https://api.chess.com/pub/player/", username, "/games/archives"))$archives
   }
 
+  # this function will parse the list of game urls and extract a json blob
   get_games <- function(y) {
-    tryCatch( {y <- jsonlite::fromJSON(y)}, error = function(x) {y <- NA})
+    y <- jsonlite::fromJSON(y)
   }
 
-  extract_pgn <- function(x){
-    tryCatch( {x <- x$games$pgn}, error = function(x) {x <- NA}) %>% as.character() %>% data.frame() %>% dplyr::mutate_if(is.factor, as.character)
 
-  }
+  convert_to_df <- function(games_list) {
 
-  clean_pgn <- function(df) {
+    # function to extract the game and moves data required for analysis
+    extract_pgn <- function(x){
+      tryCatch( {x <- x$games$pgn}, error = function(x) {x <- NA}) %>% as.character() %>% data.frame() %>% dplyr::mutate_if(is.factor, as.character)
+    }
+    pgn <- games_list %>%
+      purrr::map_df(extract_pgn)
 
-    colnames(df) <- "each_game"
-    # split column into the columns contained in the data
-    cleaned_df <- suppressWarnings(tidyr::separate(df, each_game, into = c("Event", "Site", "Date", "Round", "White", "Black", "Result", "ECO", "ECOUrl", "CurrentPosition", "Timezone",
-                                                                    "UTCDate", "UTCTime", "WhiteElo", "BlackElo", "TimeControl", "Termination", "StartTime", "EndDate", "EndTime",
-                                                                    "Link", "Moves"), sep = "]\n"))
+    # function to extract the rules of each game
+    extract_rules <- function(x){
+      tryCatch( {x <- x$games$rules}, error = function(x) {x <- NA}) %>% as.character() %>% data.frame() %>% dplyr::mutate_if(is.factor, as.character)
+    }
+
+    # function to extract the time class of each game (ie blitz, bullet, daily, etc)
+    extract_time_class <- function(x){
+      tryCatch( {x <- x$games$time_class}, error = function(x) {x <- NA}) %>% as.character() %>% data.frame() %>% dplyr::mutate_if(is.factor, as.character)
+    }
+
+    rules <- games_list %>%
+      purrr::map_df(extract_rules)
+
+    time_class <- games_list %>%
+      purrr::map_df(extract_time_class)
+
+    df <- cbind(rules, time_class, pgn) %>% data.frame()
+    colnames(df) <- c("rules", "time_class", "pgn")
+
+
+    cleaned_df <- df[grep("\\{", df$pgn),]
+
+    cleaned_df <- cleaned_df %>% dplyr::filter(.data$rules == "chess")
+    cleaned_df <- cleaned_df %>% dplyr::filter(.data$time_class %in% c("blitz", "bullet",  "daily",  "rapid"))
+    cleaned_df <- cleaned_df %>% dplyr::filter(!stringr::str_detect(.data$pgn, "Tournament"))
+    cleaned_df <- cleaned_df %>% dplyr::filter(!stringr::str_detect(.data$pgn, "club/matches"))
+
+    cleaned_df <- cleaned_df %>%
+      tidyr::separate(.data$pgn, into = c("Event", "Site", "Date", "Round", "White", "Black", "Result", "ECO", "ECOUrl", "CurrentPosition", "Timezone",
+                                          "UTCDate", "UTCTime", "WhiteElo", "BlackElo", "TimeControl", "Termination", "StartTime", "EndDate", "EndTime",
+                                          "Link", "Moves"), sep = "]\n")
+
 
     # create a vector of the variables that contains the data we need withing double quotes
     vars_to_extract <- c("Event", "Site", "Date", "Round", "White", "Black", "Result", "ECO", "ECOUrl", "CurrentPosition", "Timezone",
@@ -57,24 +79,37 @@ get_raw_game_data <- function(usernames) {
     cleaned_df <- cleaned_df %>%
       dplyr::mutate_at(vars_to_extract, extract_data) %>% dplyr::mutate_if(is.factor, as.character)
 
-    # print a message to indicate how many observations there are without analysis data
-    print(paste0("There were ", sum(is.na(cleaned_df$Moves), na.rm = T) + sum(cleaned_df$Event == "Live Chess - Chess960", na.rm = T), " records removed due to there being no analysis data"))
-
-    # filter the records that don't have analysis data for
-    cleaned_df <- cleaned_df %>% dplyr::filter(!is.na(Moves))
-    cleaned_df <- cleaned_df %>% dplyr::filter(Event != "Live Chess - Chess960")
-
+    cleaned_df <- cleaned_df %>% dplyr::mutate(Username = username)
 
   }
 
+
+
   output <- get_game_urls() %>%
     purrr::map(get_games) %>%
-    purrr::map_df(extract_pgn) %>%
-    clean_pgn()
+    convert_to_df() %>% dplyr::distinct(.keep_all = TRUE)
 
   cat("Data extracted\n")
 
 
-  return(output %>% dplyr::distinct(.keep_all = TRUE))
+  return(output)
 
+}
+
+
+#' Get Raw chess.com Game Data
+#'
+#' This function returns the raw json data for a player's or list of players'
+#' chess.com data as a data frame
+#'
+#' @param usernames A vector of a valid unsername or usernames from chess.com
+#'
+#' @import magrittr
+#' @import purrr
+#'
+#' @export
+get_raw_game_data <- function(usernames) {
+  df <- purrr::map_df(usernames, get_each_raw)
+
+  return(df)
 }
