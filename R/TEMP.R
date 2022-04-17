344c9841f7a0bd0780d543808c776db2abc015b7
dibbles21 <danwakeling7@gmail.com>
2022-04-17 20:51:56 +0100

    work on move time function

--- R/TEMP.R
+++ R/TEMP.R
@@ -1,5 +1,5 @@
-# function to extract the time stamps for a game
-lichess_game_data <- get_raw_lichess("LordyLeroy")
+# # function to extract the time stamps for a game
+# lichess_game_data <- get_raw_lichess("LordyLeroy")
 
 # function to add increment column
 add_increment <- function(games_list){
@@ -14,16 +14,18 @@ add_increment <- function(games_list){
 
 }
 
-# function to extract time control
+# function to extract time data
+
 
 test <- lichess_game_data %>%
   # remove correspondence games, as not relevant
   dplyr::filter(!(grepl("Correspondence", Event))) %>%
   add_increment()
 
+# slice(1) to test for the first game
 x <- test %>%
   dplyr::slice(1) %>%
-  dplyr::select(Moves)
+  dplyr::select(Moves, Increment)
 
 v <- stringr::str_split(x, "\\[|\\}")[[1]] %>%
   dplyr::as_tibble() %>%
@@ -31,16 +33,22 @@ v <- stringr::str_split(x, "\\[|\\}")[[1]] %>%
   dplyr::filter(grepl("clk", time)) %>%
   dplyr::mutate(time = stringr::str_remove_all(time, "%clk "),
                 time = stringr::str_remove_all(time, "\\] ")) %>%
+  dplyr::full_join(test %>%
+                     dplyr::slice(1) %>%
+                     dplyr::select(Site, Increment, White, Black),
+                   by = character()) %>%
   dplyr::mutate(colour = ifelse(
     dplyr::row_number() %% 2 == 0,
     "Black",
     "White"),
     move_number = floor((1 + dplyr::row_number()) / 2),
-    move_time = )
+    time = lubridate::seconds(lubridate::hms(time)),
+    move_time = lubridate::seconds(ifelse(dplyr::row_number() <= 2,
+                       0,
+                       Increment - time + dplyr::lag(time, 2))))
 
-ggplot2::ggplot(v,
-                ggplot2::aes(x = move_number,
-                             y = time,
-                             colour = colour)) +
-  ggplot2::geom_point()
+# ggplot2::ggplot(v,
+#                 ggplot2::aes(x = move_time,
+#                              fill = colour)) +
+#   ggplot2::geom_histogram()
 
